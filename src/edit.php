e268dc64750516ed1297bac505e09eba6dc03180
Ludvigpbf <ludvig.flyckt@gmail.com>
2023-01-10 19:25:18 +0100

    added messages divs

--- src/edit.php
+++ src/edit.php
@@ -1,12 +1,29 @@
 <?php
- 
 include 'db-conn.php';
-
 $messages = [
     "success" => "Task updated!",
     "failed" => "All fields must be filled in!",
 ];
-$id=$_GET['edit-task'];
+
+
+
+/* $stmt = $pdo->query('SELECT id, title, task FROM tasks'); */
+
+if(isset($_GET['edit-task'])){
+    $id=$_GET['edit-task'];
+
+    $sql = "SELECT id, title, task FROM tasks WHERE id=$id";
+    $run = $pdo->prepare($sql);
+    $run->execute();
+    if($run){
+        echo '<div class="message"><p class="mess">Task updated!</p></div>';
+    }else{
+        echo "Hmm.. it didnt work..";
+    }
+}
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -24,21 +41,28 @@ $id=$_GET['edit-task'];
 
 <body>
     <header><img src="../assets/images/TM-logo.png" alt="TM logo"></header>
+    <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){ ?>
     <section id="new-task">
+    
         <form id="task_form" method="POST" action="edit.php">
+        <p id="hidden" name="id"><?php echo $row['id'];?></p>
         <label class="heading" for="title">Update title</label>
-        <input name="title" id="taskTitle" class="task-text" type="text" placeholder="Add a title" maxlength="30" autocomplete="off"/><!-- count down letters -->
+        <input name="title" id="taskTitle" class="task-text" type="text" maxlength="30" autocomplete="off" value="<?php echo $row['title'];?>"/><!-- count down letters -->
         <label class="heading" for="task">Update task</label>
-        <textarea name="task" id="txt-content" cols="10" rows="2" placeholder="What do you need to do?" maxlength="150"></textarea>
+        <textarea name="task" id="txt-content" cols="10" rows="2" maxlength="150"><?php echo $row['task'];?></textarea>
         <div id="buttons"><label id="ad-picture" for="img">Update image<input type="file" id="img" name="img" accept="image/*"></label>
         <button id="ad-btn" type="submit">Update task</button></div>
     </form>
+    
 </section>
-<?php if(!empty($_POST['title']) && !empty($_POST['task'])){
+<?php } ?>
+<?php
+if(!empty($_POST['title']) && !empty($_POST['task'])){
     $title = $_POST['title'];
     $task = $_POST['task'];
+    $id = 105;
     
-    $sql = "UPDATE tasks SET title=$title, task=$task";
+    $sql = "UPDATE tasks SET title='$title', task='$task' WHERE id='$id'";
     
     $stmt=$pdo->prepare($sql);
     $stmt->execute();
