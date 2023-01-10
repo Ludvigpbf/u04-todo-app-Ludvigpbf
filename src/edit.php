8dd0884992a31ce53571bcf5c066f485609993ec
Ludvigpbf <ludvig.flyckt@gmail.com>
2023-01-10 22:10:43 +0100

    uppdatering fungerar!

--- src/edit.php
+++ src/edit.php
@@ -6,24 +6,6 @@ $messages = [
 ];
 
 
-
-/* $stmt = $pdo->query('SELECT id, title, task FROM tasks'); */
-
-if(isset($_GET['edit-task'])){
-    $id=$_GET['edit-task'];
-
-    $sql = "SELECT id, title, task FROM tasks WHERE id=$id";
-    $run = $pdo->prepare($sql);
-    $run->execute();
-    if($run){
-        echo '<div class="message"><p class="mess">Task updated!</p></div>';
-    }else{
-        echo "Hmm.. it didnt work..";
-    }
-}
-
-
-
 ?>
 
 <!DOCTYPE html>
@@ -41,34 +23,47 @@ if(isset($_GET['edit-task'])){
 
 <body>
     <header><img src="../assets/images/TM-logo.png" alt="TM logo"></header>
-    <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){ ?>
+    <?php
+    if(isset($_GET['edit-task'])){/* Här är ID't som står i url'en */
+        $id=$_GET['edit-task'];
+        $sql = "SELECT id, title, task FROM tasks WHERE id=$id";
+        $run = $pdo->prepare($sql);
+        $run->execute();
+        if($run){
+            while ($row = $run->fetch(PDO::FETCH_ASSOC)){ ?>
     <section id="new-task">
     
-        <form id="task_form" method="POST" action="edit.php">
-        <p id="hidden" name="id"><?php echo $row['id'];?></p>
+        <form id="task_form" method="POST" action="edit.php?edit-task=<?php echo $row['id'];?>">
+        <input type="hidden" name="task-id" value="<?php echo $id; ?>">
         <label class="heading" for="title">Update title</label>
         <input name="title" id="taskTitle" class="task-text" type="text" maxlength="30" autocomplete="off" value="<?php echo $row['title'];?>"/><!-- count down letters -->
         <label class="heading" for="task">Update task</label>
         <textarea name="task" id="txt-content" cols="10" rows="2" maxlength="150"><?php echo $row['task'];?></textarea>
         <div id="buttons"><label id="ad-picture" for="img">Update image<input type="file" id="img" name="img" accept="image/*"></label>
-        <button id="ad-btn" type="submit">Update task</button></div>
+        <button id="ad-btn" type="submit" name="update">Update task</button></div>
     </form>
     
 </section>
-<?php } ?>
-<?php
-if(!empty($_POST['title']) && !empty($_POST['task'])){
+<?php }} else {
+    echo '<div class="message"><p class="mess">Something went wrong.. please try again!</p></div>';
+    }
+ } 
+ if(isset($_POST['update'])){
     $title = $_POST['title'];
     $task = $_POST['task'];
-    $id = 105;
+    $id = $_POST['task-id'];
     
     $sql = "UPDATE tasks SET title='$title', task='$task' WHERE id='$id'";
     
     $stmt=$pdo->prepare($sql);
     $stmt->execute();
-?><div class="message"><p class="mess"><?php echo $messages['success'];
-}else{
-    ?></p></div><div id="hidden"><p class="mess"><?php echo $messages['failed'];}?></p></div>
+    if($sql == TRUE){
+        echo '<div class="message"><p class="mess">Task updated!</p></div>';
+    } else{
+        echo "Error:";
+    }
+}
+?>
     <footer>
         <div id="links">
         <a href="index.php">
